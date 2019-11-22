811bba2cf832de7aefdb96b7608907c4d9c93601
linmin <shawnlin0927@gmail.com>
2019-11-22 14:39:39 +0800

    Initial Commit

---
+++ Psychological test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Psychological test
+//
+//  Created by 林銘 on 2019/11/22.
+//  Copyright © 2019 林家銘. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
